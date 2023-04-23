0556f64ff9b4fdadae3135ac035ba6b55982b757
pcu <csutiperec@Csutoras-MacBook-Pro.local>
2023-04-23 10:34:42 +0200

    Added logging and moved hardc path to env variable

--- sendMail.py
+++ sendMail.py
@@ -6,45 +6,40 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 import questionAsker
 
-# Gmail API credentials
 USERNAME = os.getenv("DAILY20000UNAME")
 PASSWORD = os.getenv("DAILY20000PWD")
+LOCAL_PATH_TO_FOLDER = os.getenv("DAILY20000_PATH") or ''
 
 # Set up the SMTP server
 smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
 smtp_server.starttls()
 smtp_server.login(USERNAME, PASSWORD)
 
-with open('/home/csutiperec/daily20000/subscribedUsers.json', 'r') as f:
-    subscribers = json.load(f)
-
 # Compose the email message
-sender = USERNAME
-subject = 'Daily 20000'
 message = MIMEMultipart('related')
 
-message['Subject'] = subject
-message['From'] = sender
+with open(f'{LOCAL_PATH_TO_FOLDER}subscribedUsers.json', 'r') as f:
+    subscribers = json.load(f)
+
+message['Subject'] = 'Daily 20000'
+message['From'] = USERNAME
 message['Bcc'] = ', '.join(subscribers)
 
-with open('/home/csutiperec/daily20000/message.html', 'r') as f:
+with open(f'{LOCAL_PATH_TO_FOLDER}message.html', 'r') as f:
     html = f.read()
 
 chatGPTAnswer = questionAsker.askQuestion()
 
 html = html.replace("$ChatGPTResponse", chatGPTAnswer)
-
 text = MIMEText(html, "html")
 message.attach(text)
 
 # Attach the image file
-with open('/home/csutiperec/daily20000/20000HUF.png', 'rb') as f:
+with open(f'{LOCAL_PATH_TO_FOLDER}20000HUF.png', 'rb') as f:
     img_data = f.read()
 img_part = MIMEImage(img_data, name='20000HUF.png')
 img_part.add_header('Content-ID', '<deak')
 message.attach(img_part)
 
 smtp_server.send_message(message)
-
-# Close the SMTP server
 smtp_server.quit()
--- subscriptionService.py
+++ subscriptionService.py
@@ -5,16 +5,13 @@ import os
 import time
 from email import policy
 from email.parser import BytesParser
+from datetime import datetime
 
-# Gmail API credentials
 USERNAME = os.getenv("DAILY20000UNAME")
 PASSWORD = os.getenv("DAILY20000PWD")
-
-# Email search criteria
+LOCAL_PATH_TO_FOLDER = os.getenv("DAILY20000_PATH") or ''
 SEARCH_CRITERIA = '(UNSEEN)'
-
-# JSON file for storing subscribed users
-SUBSCRIBERS_FILE = '/home/csutiperec/daily20000/subscribedUsers.json'
+SUBSCRIBERS_FILE = f'{LOCAL_PATH_TO_FOLDER}subscribedUsers.json'
 
 while True:
     try:
@@ -22,14 +19,18 @@ while True:
         imap = imaplib.IMAP4_SSL('imap.gmail.com')
         imap.login(USERNAME, PASSWORD)
 
-        # Select the inbox
         imap.select('inbox')
-
-        # Search for new messages matching the search criteria
         status, messages = imap.search(None, SEARCH_CRITERIA)
+
+        try:
+            with open("log.txt", "r") as f:
+                log = f.read()
+        except FileNotFoundError:
+            log = ''
 
         # Process each message
         for message_id in messages[0].split():
+
             # Fetch the message data
             status, message_data = imap.fetch(message_id, '(RFC822)')
             message_raw = message_data[0][1]
@@ -39,10 +40,14 @@ while True:
             sender = parseaddr(message['From'])[1]
             body = message.get_body(preferencelist=('plain')).get_content().lower().strip()
 
+            log += f'{datetime.now().strftime("%H:%M:%S")}: New message with the message_id of {message_id} arrived from {sender}, with the content of {body}\r\n'
+
             # Check if the sender is subscribed
             with open(SUBSCRIBERS_FILE, 'r') as f:
                 subscribers = json.load(f)
             is_subscribed = sender in subscribers
+
+            log += f"{datetime.now().strftime('%H:%M:%S')}: {sender} seems to {'already be subscribed' if is_subscribed else 'be a new subscriber'}\r\n"
 
             # Process the message based on the body
             if body == 'subscribe':
@@ -50,26 +55,30 @@ while True:
                     subscribers.append(sender)
                     with open(SUBSCRIBERS_FILE, 'w') as f:
                         json.dump(subscribers, f)
-                    print(f'Subscribed {sender}')
+                    log += f'{datetime.now().strftime("%H:%M:%S")}: Subscribed {sender}\r\n'
                 else:
-                    print(f'{sender} is already subscribed')
+                    log += f'{datetime.now().strftime("%H:%M:%S")}: {sender} is already subscribed, but send another subscribe message\r\n'
             elif body == 'unsubscribe':
                 if is_subscribed:
                     subscribers.remove(sender)
                     with open(SUBSCRIBERS_FILE, 'w') as f:
                         json.dump(subscribers, f)
-                    print(f'Unsubscribed {sender}')
+                    log += f'{datetime.now().strftime("%H:%M:%S")}: Unsubscribed {sender}\r\n'
                 else:
-                    print(f'{sender} is not subscribed')
+                    log += f'{datetime.now().strftime("%H:%M:%S")}: {sender} is already unsubscribed, but send another unsubscribe message\r\n'
 
             # Mark the message as read
             imap.store(message_id, '+FLAGS', '\\Seen')
+            log += f'{datetime.now().strftime("%H:%M:%S")}: Marked message with message_is {message_id} as Seen\r\n'
 
         # Close the connection
         imap.close()
         imap.logout()
     except Exception as e:
-        print('An error occurred:', e)
+        log += f'{datetime.now().strftime("%H:%M:%S")}: An error occurred: {e}\r\n'
+    
+    with open("log.txt", "w") as f:
+        f.write(log)
     
     # Wait for 10 seconds before checking for new messages again
     time.sleep(10)
